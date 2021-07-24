29f65c8457a7f27328a599f409ef380ca416b522
pscott <scott.piriou@ledger.fr>
2021-07-24 22:07:56 +0200

    Remove sys imports

--- end_voting_phase.py
+++ end_voting_phase.py
@@ -1,5 +1,3 @@
-import sys
-
 from starkware.crypto.signature.signature import (
     pedersen_hash, private_to_stark_key, private_key_to_ec_point_on_stark_curve, get_random_private_key, verify, inv_mod_curve_size, EC_GEN, ALPHA, FIELD_PRIME, EC_ORDER, sign)
 from starkware.crypto.signature.math_utils import (
