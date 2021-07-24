f73c9807cfbe45572374dbeec806dadd1383672a
aurelien <aurelien.wery.pro@gmail.com>
2021-07-25 01:30:29 +0200

    changement de provider (metamask)

--- signGestion/get_signer_address.js
+++ signGestion/get_signer_address.js
@@ -2,18 +2,18 @@ const ethers = require('ethers');
 
 async function main() {
     const rawSignature = process.argv[2];
-    const blinded_request = process.argv[3];
+    const message = process.argv[3];
     const address_suppose_tobe = process.argv[4];
     let provider;
 
-//   window.ethereum.enable().then((provider = new ethers.providers.Web3Provider(window.ethereum)));
+  window.ethereum.enable().then((provider = new ethers.providers.Web3Provider(window.ethereum)));
 
-  provider = ethers.providers.getDefaultProvider(
-    'goerli',
-    '2DWV9SDT3WK71GRR1YYWJRF1XIAPY1NKG6'
-  );
+//   provider = ethers.providers.getDefaultProvider(
+//     'goerli',
+//     '2DWV9SDT3WK71GRR1YYWJRF1XIAPY1NKG6'
+//   );
 
-  const signerAddress = ethers.utils.verifyMessage(blinded_request, rawSignature);
+  const signerAddress = ethers.utils.verifyMessage(message, rawSignature);
 
   if (address_suppose_tobe.toString() == signerAddress.toString()) {
     console.log(0);
