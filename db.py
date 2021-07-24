005b9b122d6211edc971c8157bd8cd060514ec47
LouGel <l.gelinet@protonmail.ch>
2021-07-25 01:23:18 +0200

    db.py

--- db.py
+++ db.py
@@ -1,24 +1,15 @@
 import json , csv
 
-def makeDB () :
-    with open('humanList.json',) as readFile :
-        data = json.load(readFile)
-        with open('VotersLeft.csv', 'w') as writeFile :
-            w = csv.writer(writeFile)
-            for k in range(len(data)) :
-                w.writerow([data[k].lower()])
 
 def try_vote(adress) :
-    members = '[\'' + adress.lower() + '\']'
+    members = adress.lower()
     ret = False
     lines =  list()
-    with open('VotersLeft.csv', 'r') as readFile:
-        reader = csv.reader(readFile)
+    with open('humanList.json', 'r') as readFile:
+        reader = json.load(readFile)
         for row in reader:
             if str(row) != str(members) :
                 lines.append(row)
-            else :
-                ret = True
     if ret :
         with open('VotersLeft.csv', 'w') as writeFile:
             writer = csv.writer(writeFile)
