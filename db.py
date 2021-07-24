246d20563739ee1c7ed13405831723030e36428c
pscott <scott.piriou@ledger.fr>
2021-07-25 01:53:06 +0200

    Reset db to earlier version

--- db.py
+++ db.py
@@ -1,16 +1,28 @@
-import json , csv
+import json
+import csv
 
 
-def try_vote(adress) :
-    members = adress.lower()
+def makeDB():
+    with open('humanList.json',) as readFile:
+        data = json.load(readFile)
+        with open('VotersLeft.csv', 'w') as writeFile:
+            w = csv.writer(writeFile)
+            for k in range(len(data)):
+                w.writerow([data[k].lower()])
+
+
+def tryvote(adress):
+    members = '[\'' + adress.lower() + '\']'
     ret = False
-    lines =  list()
-    with open('humanList.json', 'r') as readFile:
-        reader = json.load(readFile)
+    lines = list()
+    with open('VotersLeft.csv', 'r') as readFile:
+        reader = csv.reader(readFile)
         for row in reader:
-            if str(row) != str(members) :
+            if str(row) != str(members):
                 lines.append(row)
-    if ret :
+            else:
+                ret = True
+    if ret:
         with open('VotersLeft.csv', 'w') as writeFile:
             writer = csv.writer(writeFile)
             writer.writerows(lines)
