0799bb7cbb969f30d99af47e8698e3bdbeec5e16
pscott <scott.piriou@ledger.fr>
2021-07-25 14:02:18 +0200

    Add launch_command command

--- local.py
+++ local.py
@@ -3,11 +3,12 @@ from generate_keypair import generate_keypair
 from blind import blind
 from sign_token import sign_token
 from unblind import unblind
+from commit import commit
 import db
 import requests
 import flask
 from flask_cors import CORS
-from shared import LIVE_DEMO
+from shared import LIVE_DEMO, CHECK_POH, print_output, launch_command
 import subprocess
 
 SERV_URL = "http://192.168.106.112:5000"
@@ -29,7 +30,7 @@ def generate_keys():
 # Utility function that calls a node script to verify if the `signature` is indeed `message` signed by `poh_address`.
 # Used to basically verify that the user is indeed the owner of poh_address.
 def verify_sig(signature, message, poh_address):
-    verif_process = subprocess.run(
+    verif_process = launch_command(
         ['node', 'signGestion/get_signer_address.js', str(signature), str(message), str(poh_address)])
     return verif_process.returncode == 0
 
@@ -42,7 +43,9 @@ def get_serv_pub_key():
 
 def get_contract_address():
     contract_address_req = requests.get(SERV_URL + '/api/get_contract_address')
-    contract_address = int(contract_address_req.json()['contract_address'])
+    req_json = contract_address_req.json()
+    print(req_json)
+    contract_address = req_json['contract_address']
     return contract_address
 
 
@@ -67,8 +70,8 @@ def generate_commit_token():
         return "Error: invalid signature", 204
 
     # check that user is actually in the POH and has not already generated a commit token
-    # if db.try_vote(poh_address) == False:
-    #     return "Error: already voted or not in POH", 205
+    if CHECK_POH and db.try_vote(poh_address) == False:
+        return "Error: already voted or not in POH", 205
 
     (blinded_request, blinded_factor) = blind(int(public_key))
 
@@ -89,15 +92,40 @@ def generate_commit_token():
     return jsonify([{'commit_token': str(commit_token)}])
 
 
+@ app.route('/api/commit', methods=['POST'])
+def commit_to_token():
+    data = request.get_json()
+    print(data)
+    if 'private_key' not in data:
+        return "Error: missing private key", 201
+    if 'commit_token' not in data:
+        return "Error: missing commit token", 203
+
+    private_key = int(data['private_key'])
+    commit_token = int(data['commit_token'])
+
+    (public_key, r, s) = commit(private_key, commit_token)
+    contract_addr = get_contract_address()
+
+    if LIVE_DEMO:
+        res = launch_command(['starknet',  'invoke', '--address', contract_addr,
+                              '--abi', 'contract_abi.json', '--network', 'alpha', '--function', 'commit', '--inputs', str(public_key), str(commit_token), str(r), str(s)])
+        print_output(res)
+        if res.returncode != 0:
+            print(res.stderr.decode('utf-8'))
+            return "Error executing starknet call: exited with {}".format(res.returncode), 204
+    return "OK"
+
+
 @ app.route('/api/get_result', methods=['GET'])
 def get_result():
-    contract_addr = get_contract_address()
     if LIVE_DEMO:
-        res = subprocess.run(['starknet',  'call', '--address', contract_addr,
-                              '--abi', 'contract_abi.json', '--function', 'get_result'])
+        contract_addr = get_contract_address()
+        res = launch_command(['starknet',  'call', '--address', contract_addr,
+                              '--abi', 'contract_abi.json', '--network', 'alpha', '--function', 'get_result'])
         if res.returncode != 0:
             return "Error executing starknet call: exited with {}".format(res.returncode), 201
-    return True
+    return "OK"
 
 
 @ app.route('/', methods=['GET'])
