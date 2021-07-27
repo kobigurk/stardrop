7c05ea0d610c8fc4579735c30eeb76813acf89dd
pscott <scott.piriou@ledger.fr>
2021-07-27 20:20:44 +0200

    Add start_local and start_server scripts

---
+++ start_local.h
@@ -0,0 +1,1 @@
+docker-compose -f docker/local.docker-compose.yml up --build && sudo docker-compose -f docker/local.docker-compose.yml down
---
+++ start_server.sh
@@ -0,0 +1,1 @@
+docker-compose -f docker/server.docker-compose.yml up --build && sudo docker-compose -f docker/server.docker-compose.yml down
