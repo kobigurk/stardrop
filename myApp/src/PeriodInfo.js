0d0c45f89031f05bc00b5c2f9333174932209782
pscott <scott.piriou@ledger.fr>
2021-07-29 18:21:41 +0200

    Info about POH checking

--- myApp/src/PeriodInfo.js
+++ myApp/src/PeriodInfo.js
@@ -4,7 +4,7 @@ import './PeriodInfo.scss';
 const INFOS = [
 	'The server is deploying the smart contract on Starknet.',
 	'The server sent the initialization transaction to the smart contract. The server is waiting for the transaction to be confirmed by Starknet.',
-	'Users can now register to the next voting round.',
+	'Users can now register to the next voting round. We removed the Proof of Humanity check for the demo, but your address would be checked here.',
 	'The smart contract is ending the registration period. It will no longer accept new registrations.',
 	'The server sent its private key to the smart contract. The server is waiting for the transaction to be confirmed by Starknet',
 	'Users who registered during the registration period can now cast their votes!',
